a6f14198e82a90a630c9030c5a124e2651bdd933
MD RUBEL <hello@mdrubel.net>
2025-05-03 23:40:06 +0600

    package name fix

--- Package.swift
+++ Package.swift
@@ -4,19 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "KahfAdsSDK",
+    name: "KahfAdSDK",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "KahfAdsSDK",
-            targets: ["KahfAdsSDK"]
+            name: "KahfAdSDK",
+            targets: ["KahfAdSDK"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
-            name: "KahfAdsSDK",
+            name: "KahfAdSDK",
             path: "KahfAdSDK.xcframework"
         ),
     ]
