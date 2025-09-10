bbf322dbc14e2d3140be594289313f34694e3ef8
shafayat <shafayathossainkhan@gmail.com>
2025-09-10 14:20:24 +0600

    Dependencies updated

--- Sources/KahfAdsSDK/KahfAdsSDK.swift
+++ Sources/KahfAdsSDK/KahfAdsSDK.swift
@@ -1,7 +1,7 @@
 // This is a wrapper target that re-exports the binary framework
 // and ensures all dependencies are available to consuming apps
 
-@_exported import KahfAdSDK
+
 @_exported import Gifu
 @_exported import Nuke
 @_exported import KMPNativeCoroutinesCore
